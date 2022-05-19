e2e321847da5104a9208b6b469c0b1e0172c944e
Joakim Rosenfeldt Pedersen <joakim@mellonn.com>
2022-05-19 10:24:29 +0200

    Finished up the webscraber

--- mellonnSpeakWeb/src/app/shared/language-service/language.service.ts
+++ mellonnSpeakWeb/src/app/shared/language-service/language.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Storage } from 'aws-amplify';
 import { parse } from 'node-html-parser';
 
 @Injectable({
@@ -11,29 +12,52 @@ export class LanguageService {
   constructor() { }
 
   async getLanguages() {
-    const result = await fetch('https://docs.aws.amazon.com/transcribe/latest/dg/supported-languages.html');
-    const text = await result.text();
-    const doc = parse(text);
+    try {
+      const result = await fetch('https://docs.aws.amazon.com/transcribe/latest/dg/supported-languages.html');
+      const text = await result.text();
+      const doc = parse(text);
 
-    const tBody = doc.getElementsByTagName('table');
-    const tr = tBody[0].getElementsByTagName('tr');
+      const tBody = doc.getElementsByTagName('table');
+      const tr = tBody[0].getElementsByTagName('tr');
 
-    for(let row of tr) {
-      let language: string;
-      const columns = row.getElementsByTagName('td');
-      try {
-        const a = columns[0].getElementsByTagName('a')[0];
-        if (columns[0].text.includes(',')) {
-          const variation = columns[0].text.replace(RegExp('[^,A-Za-z0-9]', 'g'), '');
-          const temp = variation.split(',');
-          language = temp[0] + ' (' + temp[1] + ')';
-        } else {
-          const temp = a.text.replace(RegExp('[^,A-Za-z0-9]', 'g'), '');
-          language = temp;
+      let i: number = 0;
+
+      while (this.languageCodeList.length == 0 && i < 5) {
+        for(let row of tr) {
+          let language: string;
+          const columns = row.getElementsByTagName('td');
+          try {
+            const a = columns[0].getElementsByTagName('a')[0];
+            if (columns[0].text.includes(',')) {
+              const variation = columns[0].text.replace(RegExp('[^,A-Za-z0-9]', 'g'), '');
+              const temp = variation.split(',');
+              language = temp[0] + ' (' + temp[1] + ')';
+            } else {
+              const temp = a.text.replace(RegExp('[^,A-Za-z0-9]', 'g'), '');
+              language = temp;
+            }
+            this.languageList.push(language);
+            this.languageCodeList.push(columns[1].text);
+          } catch {
+          }
         }
-        this.languageList.push(language);
-        this.languageCodeList.push(columns[1].text);
-      } catch {
+        i++;
+      }
+      if (i >= 5) throw 'timeout';
+    } catch (err) {
+      try {
+        const url = await Storage.get('data/languages.json');
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+          },
+        });
+        const json = await response.json();
+        this.languageList = json.languages;
+        this.languageCodeList = json.languageCodes;
+      } catch (err) {
+        console.log('Error while getting backup language list: ' + err);
       }
     }
   }
