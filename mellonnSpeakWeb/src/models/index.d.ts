060fadcc8755e0ea79ebc9408caab8d5294fa9f8
Joakim Rosenfeldt Pedersen <joakim@mellonn.com>
2022-10-27 09:52:38 +0200

    First commit

--- mellonnSpeakWeb/src/models/index.d.ts
+++ mellonnSpeakWeb/src/models/index.d.ts
@@ -1,8 +1,6 @@
-import { ModelInit, MutableModel, PersistentModelConstructor } from "@aws-amplify/datastore";
-
-
-
-
+import { ModelInit, MutableModel } from "@aws-amplify/datastore";
+// @ts-ignore
+import { LazyLoading, LazyLoadingDisabled, AsyncCollection } from "@aws-amplify/datastore";
 
 type SettingsMetaData = {
   readOnlyFields: 'createdAt' | 'updatedAt';
@@ -16,7 +14,7 @@ type RecordingMetaData = {
   readOnlyFields: 'createdAt' | 'updatedAt';
 }
 
-export declare class Settings {
+type EagerSettings = {
   readonly id: string;
   readonly themeMode: string;
   readonly languageCode: string;
@@ -24,22 +22,49 @@ export declare class Settings {
   readonly primaryCard?: string | null;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
-  constructor(init: ModelInit<Settings, SettingsMetaData>);
-  static copyOf(source: Settings, mutator: (draft: MutableModel<Settings, SettingsMetaData>) => MutableModel<Settings, SettingsMetaData> | void): Settings;
 }
 
-export declare class Version {
+type LazySettings = {
+  readonly id: string;
+  readonly themeMode: string;
+  readonly languageCode: string;
+  readonly jumpSeconds: number;
+  readonly primaryCard?: string | null;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type Settings = LazyLoading extends LazyLoadingDisabled ? EagerSettings : LazySettings
+
+export declare const Settings: (new (init: ModelInit<Settings, SettingsMetaData>) => Settings) & {
+  copyOf(source: Settings, mutator: (draft: MutableModel<Settings, SettingsMetaData>) => MutableModel<Settings, SettingsMetaData> | void): Settings;
+}
+
+type EagerVersion = {
   readonly id: string;
   readonly date: string;
   readonly recordingID: string;
   readonly editType: string;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
-  constructor(init: ModelInit<Version, VersionMetaData>);
-  static copyOf(source: Version, mutator: (draft: MutableModel<Version, VersionMetaData>) => MutableModel<Version, VersionMetaData> | void): Version;
 }
 
-export declare class Recording {
+type LazyVersion = {
+  readonly id: string;
+  readonly date: string;
+  readonly recordingID: string;
+  readonly editType: string;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type Version = LazyLoading extends LazyLoadingDisabled ? EagerVersion : LazyVersion
+
+export declare const Version: (new (init: ModelInit<Version, VersionMetaData>) => Version) & {
+  copyOf(source: Version, mutator: (draft: MutableModel<Version, VersionMetaData>) => MutableModel<Version, VersionMetaData> | void): Version;
+}
+
+type EagerRecording = {
   readonly id: string;
   readonly name: string;
   readonly date?: string | null;
@@ -54,6 +79,27 @@ export declare class Recording {
   readonly labels?: (string | null)[] | null;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
-  constructor(init: ModelInit<Recording, RecordingMetaData>);
-  static copyOf(source: Recording, mutator: (draft: MutableModel<Recording, RecordingMetaData>) => MutableModel<Recording, RecordingMetaData> | void): Recording;
+}
+
+type LazyRecording = {
+  readonly id: string;
+  readonly name: string;
+  readonly date?: string | null;
+  readonly description?: string | null;
+  readonly fileKey?: string | null;
+  readonly fileName?: string | null;
+  readonly fileUrl?: string | null;
+  readonly speakerCount: number;
+  readonly languageCode?: string | null;
+  readonly versions: AsyncCollection<Version>;
+  readonly interviewers?: (string | null)[] | null;
+  readonly labels?: (string | null)[] | null;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type Recording = LazyLoading extends LazyLoadingDisabled ? EagerRecording : LazyRecording
+
+export declare const Recording: (new (init: ModelInit<Recording, RecordingMetaData>) => Recording) & {
+  copyOf(source: Recording, mutator: (draft: MutableModel<Recording, RecordingMetaData>) => MutableModel<Recording, RecordingMetaData> | void): Recording;
 }
